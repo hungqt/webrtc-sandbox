8efc2df22d0909cb40fa58009f238783810dc2a5
hungqt <hungqt@users.noreply.github.com>
2017-06-13 15:26:47 +0200

    moved gitignore to root, and initialized LocalPeerConnection project

---
+++ LocalPeerConnection/server/server.js
@@ -0,0 +1,16 @@
+/* SIMPLE EXPRESS SERVER TO RUN THE MEDIA STREAM APP ON LOCALHOST*/
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+//Specify localhost port to 8000
+app.set('port', 8000);
+
+app.use(express.static('../public'));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Peer Connection example app listening on port ' + port);
+});
