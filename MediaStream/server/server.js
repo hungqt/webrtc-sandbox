e09a667fd1ec64714377b19c12ca7db50fc81a1a
hungqt <hungqt@users.noreply.github.com>
2017-06-13 12:16:46 +0200

    Rewrite server from basic node to express

--- MediaStream/server/server.js
+++ MediaStream/server/server.js
@@ -1,20 +1,14 @@
-var http = require("http");
-var fs = require("fs");
-
 /* SIMPLE EXPRESS SERVER TO RUN THE MEDIA STREAM APP ON LOCALHOST*/
 
-function onRequest(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  fs.readFile('../client/index.html', null, function(error, data){
-    if(error){
-      response.writeHead(404);
-      response.write('File not found!');
-    } else {
-      response.write(data);
-    }
-    response.end()
-  });
-}
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
+});
 
 //Specify localhost port to 8000
-http.createServer(onRequest).listen(8000);
+app.listen(8000, function () {
+  console.log('MediaStream example app listening on port 8000');
+})
