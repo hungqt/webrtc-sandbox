e90876ffb93508faf1f73598b1ad0ba1d19f6b20
hungqt <hqthieu@gmail.com>
2017-06-15 15:55:34 +0200

    Socket io chat app project done

--- Chatio/server.js
+++ Chatio/server.js
@@ -17,6 +17,28 @@ io.sockets.on('connection', function(socket){
   console.log('Connected: %s sockets conected', connections.length);
 
   // Disconnect
-  connections.splice(connections.indexOf(socket), 1);
-  console.log('Disconnected: %s sockets connected', connections.length);
+  socket.on('disconnect', function(data) {
+
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  // Send Message
+  socket.on('send message', function(data) {
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  // New user
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
 });
