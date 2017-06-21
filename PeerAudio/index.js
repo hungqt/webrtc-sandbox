979ca4ac7fc6115238ba72223ffaba4feb454340
hungqt <hqthieu@gmail.com>
2017-06-21 15:10:20 +0200

    added socket connection and disconnect

--- PeerAudio/index.js
+++ PeerAudio/index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var connections = [];
 
 server.listen(process.env.PORT || 8000);
 console.log('Server running at port ' + process.env.PORT || 8000);
@@ -13,5 +14,24 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
-  
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  // Convenience function to log server messages on the client (client listens to it on socket.on('log'))
+  function log() {
+    var array = ['Message from server: '];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets connected', connections.length);
+  });
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+    
+
+  })
 });
