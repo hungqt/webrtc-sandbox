69d8a0a4328cb7f9d43d21582104946439679642
hungqt <hqthieu@gmail.com>
2017-06-22 08:26:39 +0200

    socket connection

--- PeerAudio/js/main.js
+++ PeerAudio/js/main.js
@@ -8,3 +8,14 @@
 
 var configuration = null;
 console.log("test");
+
+/*******************************************************************************/
+// Signaling Server //
+
+//Connect to the signaling server
+var socket = io.connect();
+
+// Listens to the servers console logs
+socket.on('log', function(array) {
+  console.log.apply(console, array);
+});
