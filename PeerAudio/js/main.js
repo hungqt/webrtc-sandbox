13f08478daad21b559aa2bb9a1638b6e99c989f0
hungqt <hqthieu@gmail.com>
2017-07-05 11:29:53 +0200

    Deleted useless hide show functions

--- PeerAudio/js/main.js
+++ PeerAudio/js/main.js
@@ -327,16 +327,6 @@ function saveAudioClip(audioblob) {
   }
 }
 
-
-// Show and hide buttons whenever user media is loaded
-function show() {
-
-}
-
-function hide() {
-
-}
-
 function randomToken() {
   return Math.floor((1 + Math.random()) * 1e16).toString(16).substring(1);
 }
